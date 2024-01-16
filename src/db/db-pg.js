290f0b93c235024ba65f568e9697aecc9b911d3b
abhinav3254 <abhinav3254@gmail.com>
2024-01-16 22:18:12 +0530

    comments

--- src/db/db-pg.js
+++ src/db/db-pg.js
@@ -1,5 +1,10 @@
 const { Client } = require('pg');
 
+
+/**
+ * setting up database config
+ * @author abhinav3254
+ */
 const client = new Client({
     user: 'postgres',
     password: 'root3254',
@@ -8,6 +13,12 @@ const client = new Client({
     port: 5432,
 });
 
+
+/**
+ * checking whether application is connected to database or not
+ * 
+ * @author abhinav3254
+ */
 client.connect().then(() => {
     console.log('Connected to PostgreSQL database!');
 }).catch((err) => {
