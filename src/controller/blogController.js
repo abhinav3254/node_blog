eb70577cadc30a3e38f0889916154803d17fa2c9
abhinav3254 <abhinav3254@gmail.com>
2024-01-17 22:22:24 +0530

    blog controller

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const authenticateJWT = require('../middleware/jwtMiddleware')
-
+const authenticateJWT = require('../middleware/jwtMiddleware');
+const blogService = require('../service/blogService');
 
 router.get('/', authenticateJWT, async (req, res) => {
-    res.status(200).send({ message: 'get all the blogs' });
+    const service = await blogService.getAllTheBlogs();
+    res.status(200).send({ message: 'get all the blogs', service: service });
+});
+
+
+router.post('/post', authenticateJWT, async (req, res) => {
+    const service = await blogService.postANewBlog();
+    res.status(201).send({ message: 'want to insert a new blog', service: service });
 });
 
 module.exports = router;
